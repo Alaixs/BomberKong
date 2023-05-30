546a47ef5c9109f72652d25489b2756cde12b33b
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2023-05-30 11:29:12 +0200

    opti widget.cpp

--- bomberkong/widget.cpp
+++ bomberkong/widget.cpp
@@ -1,64 +1,64 @@
+// includes the necessary header files
 #include "widget.h"
 #include "ui_widget.h"
 
 #include "input.h"
 
-
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
+: QWidget(parent)
+, ui(new Ui::Widget)
 {
-    ui->setupUi(this);
+ui->setupUi(this);
 
-    // updates the games every 16ms
-    connect(&timer, SIGNAL( timeout() ),
-            this, SLOT( gameUpdate() ));
-    timer.start(16); // Approx. 60 FPS
+// updates the game every 16ms
+connect(&timer, SIGNAL(timeout()), this, SLOT(gameUpdate()));
+timer.start(16); // Approx. 60 FPS
 }
-
 
 Widget::~Widget()
 {
-    delete ui;
+delete ui;
 }
 
-
 // Updating the Input class states
-void Widget::keyPressEvent(QKeyEvent * ev) { Input::keyPressedEvent(ev); }
-void Widget::keyReleaseEvent(QKeyEvent * ev) { Input::keyReleasedEvent(ev); }
+void Widget::keyPressEvent(QKeyEvent *ev)
+{
+Input::keyPressedEvent(ev);
+}
 
+void Widget::keyReleaseEvent(QKeyEvent *ev)
+{
+Input::keyReleasedEvent(ev);
+}
 
 void Widget::gameUpdate()
 {
-    std::vector<Entity*>::iterator it = entities.begin();
-    while (it != entities.end())
-    {
-        // TODO : remove the entity if it is inactive
-        (*it)->update();
-        it++;
-    }
-
-    // Draw a frame
-    repaint(0, 0, 800, 600);
+std::vector<Entity*>::iterator it = entities.begin();
+while (it != entities.end())
+{
+// TODO: remove the entity if it is inactive
+(*it)->update();
+it++;
 }
 
+// Draw a frame
+repaint(0, 0, 800, 600);
+}
 
 void Widget::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
 
-    // Background
+    // Fill the background with a color
     painter.fillRect(
         0, 0, 800, 600,
-        QBrush(QColor(216,197,150))
+        QBrush(QColor(216, 197, 150))
     );
 
-    std::vector<Entity*>::iterator it = entities.begin();
-    while (it != entities.end())
+    for (std::vector<Entity*>::iterator it = entities.begin(); it != entities.end(); ++it)
     {
+        // Draw the entity
         (*it)->draw(&painter);
-        it++;
     }
-
 }
 
