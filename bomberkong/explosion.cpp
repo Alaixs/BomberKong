3b2d90491b369a75ca66a2d019da9692ec57803e
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2023-06-03 16:53:47 +0200

    test

--- bomberkong/explosion.cpp
+++ bomberkong/explosion.cpp
@@ -1,8 +1,10 @@
 #include "explosion.h"
+#include <QDebug>
 
 Explosion::Explosion(int posX, int posY)
     : Entity(posX, posY)
 {
+    qDebug() << "construct principale" ;
     sprite.load("://assets/sprites/t_explosion.png");
     animation.setInterval(2);
     animation.play(0, 6);
@@ -12,6 +14,7 @@ Explosion::Explosion(int posX, int posY)
 Explosion::Explosion(Coordinate position)
     : Entity(position)
 {
+        qDebug() << "construct coordinate" ;
     sprite.load("://assets/sprites/t_explosion.png");
     animation.setInterval(2);
     animation.play(0, 6);
@@ -20,6 +23,7 @@ Explosion::Explosion(Coordinate position)
 
 void Explosion::update()
 {
+            qDebug() << "Update" ;
     animation.update();
     timer--;
     if (timer == 0)
@@ -31,6 +35,7 @@ void Explosion::update()
 
 void Explosion::draw(QPainter * painter)
 {
+   qDebug() << "draw painter" ;
     painter->drawPixmap(
         QRect(pos.x, pos.y, 48, 48),
         sprite,
@@ -41,5 +46,6 @@ void Explosion::draw(QPainter * painter)
 
 QRect Explosion::getRect()
 {
+            qDebug() << "getrect baby" ;
     return QRect(pos.x, pos.y, 48, 48);
 }
