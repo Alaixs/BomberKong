dad7e9ca4ab8aeae116af0d163ec45b6901ea0b8
yacintho nicolas <yacintho.nicolas@etudiant.univ-lr.fr>
2023-06-07 11:47:47 +0200

    Add comment on tutorial.h

--- bomberkong/tutorial.h
+++ bomberkong/tutorial.h
@@ -8,24 +8,72 @@
 #include <list>
 #include <QWidget>
 
-
+/**
+ * @brief The class of tutorial screen
+ */
 class Tutorial : public Game
 {
 
 public:
+    /**
+     * @brief Create new Tutorial screen
+     * @param widget : the widget of the game
+     */
     Tutorial(QWidget* widget);
+
+    /**
+     * @brief Destructor of Tutorial
+     */
     ~Tutorial();
 
+    /**
+     * @brief Update Tutorial screen (position, sprite, etc)
+     */
     void update();
-    void draw(QPainter* painter);
 
+    /**
+     * @brief Display the actual Tutorial screen
+     * @param painter : the tools to display
+     */
+    void draw(QPainter * painter);
+
+
+    /**
+     * @brief Create new entity on Tutorial screen
+     * @param entity : the entity to be added
+     */
     void createEntity(Entity* entity);
+
+    /**
+     * @brief Delete all entities on Tutorial screen
+     */
     void deleteAllEntity();
 
+    /**
+     * @brief Switch to win menu
+     */
     void win();
+
+    /**
+     * @brief Switch to loose menu
+     */
     void loose();
+
+    /**
+     * @brief Switch to alternative win menu
+     */
     void alternative();
+
+    /**
+     * @brief Create new explosion
+     * @param posX : the x value
+     * @param posY : the y value
+     */
     void createExplosion(int posX, int posY);
+
+    /**
+     * @brief Reload the Tutorial menu
+     */
     void restart();
 
     int wMap;
